841febe354ff17c71d707886053b3af73e959cd2
lightfish <karakapi@outlook.com>
2016-10-10 12:46:06 +0800

    压缩jar工具基本完成,下一步完善jdk原有的 jar包过滤

--- compiler/src/main/java/ReduceRT.java
+++ compiler/src/main/java/ReduceRT.java
@@ -1,145 +1,92 @@
-import lombok.val;
+import lombok.Cleanup;
+import org.apache.commons.io.FileUtils;
 
-import java.io.*;
-import java.nio.file.FileSystem;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import static java.lang.System.out;
 
 public class ReduceRT {
-    // 读取路径,copy
-    public static void dealClass(String needfile, String source, String object) throws Exception {
-        String sdir = source.replaceAll("\\\\", "/");
-        String odir = object.replaceAll("\\\\", "/");
-        File usedclass = new File(needfile.replaceAll("\\\\", "/"));
+    public static void dealClass(Stream<String> stream, FileSystem jar, String odir) {
         final AtomicLong counter = new AtomicLong(0);
-        if (usedclass.canRead()) {
-            try (InputStream is = new FileInputStream(new File(needfile)); InputStreamReader ir = new InputStreamReader(is, "UTF-8"); LineNumberReader reader = new LineNumberReader(ir)) {
-                Stream<String> result = reader.lines()
-                        .filter((s) ->
-                                !s.startsWith("[Loaded com.sun") &&
-                                        s.startsWith("[Loaded") &&
-                                        !s.startsWith("[Opened ") &&
-                                        !"".equals(s) && !s.startsWith("[Loaded java") &&
-                                        !s.startsWith("[Loaded sun") &&
-                                        !s.startsWith("[Loaded jdk") &&
-                                        !s.contains("$$Lambda$"))
-                        .map(s -> s.substring("[Loaded ".length(), s.indexOf(" from")).trim().replaceAll("\\.", "/") + ".class")
-                        .distinct();
-
-                result.forEach((n) -> {
-                    try {
-                        String s = sdir + n;
-                        String o = odir + n;
-                        String dir = o.substring(0, s.lastIndexOf("/") + 1);
-                        File file = new File(dir);
-                        if (!file.exists()) {
-                            boolean res = file.mkdirs();
-                            out.println("创建文件夹" + file + ":" + res);
-                        }
-                        out.println(s + ";" + o);
-                        Files.copy(Paths.get(s), Paths.get(o));
-                        counter.getAndIncrement();
-                    } catch (Exception e) {
-                        out.println("失败:" + n);
-                        e.printStackTrace();
+        Stream<String> result = stream.filter((s) -> {
+            out.println(s);
+            return !s.startsWith("[Loaded com.sun") &&
+                    s.startsWith("[Loaded") &&
+                    !s.startsWith("[Opened ") &&
+                    !"".equals(s) && !s.startsWith("[Loaded java") &&
+                    !s.startsWith("[Loaded sun") &&
+                    !s.startsWith("[Loaded jdk") &&
+                    !s.contains("$$Lambda$");
+        })
+                .map(s -> "/" + s.substring("[Loaded ".length(), s.indexOf(" from")).trim().replaceAll("\\.", "/") + ".class")
+                .distinct();
+        result.forEach((n) -> {
+            Path c = jar.getPath(n);
+            if (Files.exists(c)) {
+                try {
+                    String o = odir + n;
+                    String dir = o.substring(0, o.lastIndexOf("/") + 1);
+                    File file = new File(dir);
+                    if (!file.exists()) {
+                        boolean res = file.mkdirs();
+                        out.println("创建文件夹" + file + ":" + res);
                     }
-                });
-                out.println("=> class个数:" + (counter.get() - 1L));
+                    out.println(c + " -> " + o);
+                    Files.copy(c, Paths.get(o));
+                    counter.getAndIncrement();
+                } catch (Exception e) {
+                    out.println("失败:" + n);
+                    e.printStackTrace();
+                }
+            } else {
+                out.println("文件不匹配" + c);
             }
-        } else {
-            out.println("目录不能读取" + needfile);
-        }
-/*        LongAdder s=new LongAdder();
-        ProcessBuilder p= new ProcessBuilder();
-        p.command()*/
+        });
+        out.println("=> class个数:" + (counter.get()));
+    }
+
+
+    static void cmd(String cmd, Consumer<Stream> fun) throws Exception {
+        Process process = Runtime.getRuntime().exec(cmd);
+        @Cleanup InputStream in = process.getInputStream();
+        @Cleanup InputStreamReader ir = new InputStreamReader(in, StandardCharsets.UTF_8);
+        @Cleanup LineNumberReader input = new LineNumberReader(ir);
+        fun.accept(input.lines());
+        process.waitFor();
     }
 
     public static void main(String[] args) throws Exception {
-        String jarPath = "L:\\\\MY\\\\staticBlog\\\\target\\\\staticBlog-0.1-SNAPSHOT.jar".replaceAll("\\\\", "/");
+        String jarPath = "L:\\\\MY\\\\staticBlog\\\\target\\\\staticBlog-0.1-SNAPSHOT.jar";
+        String jarArgs = " \"D:\\Users\\karakapi\\zhuomian\\static - 副本\"";
+        _main(new String[]{jarPath, jarArgs});
+    }
+
+    public static void _main(String[] args) throws Exception {
+        String jarPath = args[0].replaceAll("\\\\", "/");
+        String jarArgs = args[1].replaceAll("\\\\", "/");
         Path workspace = Files.createTempDirectory(Paths.get(jarPath.substring(0, jarPath.lastIndexOf("/"))), "simplifyjre");
-        Path ClassUsedList = Paths.get(workspace + "/ClassUsedList.txt");
-        String cmd = "java -jar -verbose:class " + jarPath + "> " + ClassUsedList;
-        val prpcess = Runtime.getRuntime().exec(cmd);
-        ///////////////////////////////////////
+        out.println(workspace);
+        String cmd = "java -jar -verbose:class  " + jarPath + jarArgs;
+        out.println(cmd);
+        String odir = workspace.toString();
+        File file = Paths.get(odir).toFile();
+        FileUtils.deleteDirectory(file);
+        if (!file.mkdirs()) throw new Exception("创建文件夹:" + file.toString() + "失败");
         FileSystem jar = FileSystems.newFileSystem(Paths.get(jarPath), null);
-        jar.
-                new JarFile(jarPath).stream().map((s) -> s).forEach((s) -> {
-            try {
-                out.println(s.getExtra());
-            } catch (Exception e) {
-
-            }
-
-
-        });
-
-        prpcess.waitFor();
-/*
-
-        try {
-            String needfile = "L:\\MY\\staticBlog\\target\\ClassUsedList.txt";
-            String sdir = "L:\\MY\\staticBlog\\target\\rt\\".replaceAll("\\\\", "/");
-            String odir = "L:\\MY\\staticBlog\\target\\rt1\\".replaceAll("\\\\", "/");
-            File file = Paths.get(odir).toFile();
-            delFolder(odir);
-            file.mkdirs();
-            dealClass(needfile, sdir, odir);
-            *//*FileUtils.copyDirectory(Paths.get(sdir+"/META-INF").toFile(),Paths.get(odir+"/META-INF").toFile());*//*
-            String MANIFEST = sdir + "/META-INF/MANIFEST.MF";
-            String buildJar = "jar cvfmn classes.jar " + MANIFEST + " -C " + odir + "/ .";
-            out.println(buildJar);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-    }
-
-//删除文件夹
-//param folderPath 文件夹完整绝对路径
-
-    public static void delFolder(String folderPath) {
-        try {
-            delAllFile(folderPath); //删除完里面所有内容
-            String filePath = folderPath;
-            filePath = filePath.toString();
-            java.io.File myFilePath = new java.io.File(filePath);
-            myFilePath.delete(); //删除空文件夹
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //删除指定文件夹下所有文件
-//param path 文件夹完整绝对路径
-    public static boolean delAllFile(String path) {
-        boolean flag = false;
-        File file = new File(path);
-        if (!file.exists()) {
-            return flag;
-        }
-        if (!file.isDirectory()) {
-            return flag;
-        }
-        String[] tempList = file.list();
-        File temp = null;
-        for (int i = 0; i < tempList.length; i++) {
-            if (path.endsWith(File.separator)) {
-                temp = new File(path + tempList[i]);
-            } else {
-                temp = new File(path + File.separator + tempList[i]);
-            }
-            if (temp.isFile()) {
-                temp.delete();
-            }
-            if (temp.isDirectory()) {
-                delAllFile(path + "/" + tempList[i]);//先删除文件夹里面的文件
-                delFolder(path + "/" + tempList[i]);//再删除空文件夹
-                flag = true;
-            }
-        }
-        return flag;
+        cmd(cmd, s -> dealClass(s, jar, odir));
+        Files.createDirectory(Paths.get(odir + "/META-INF"));
+        Files.copy(jar.getPath("/META-INF/MANIFEST.MF"), Paths.get(odir + "/META-INF/MANIFEST.MF"));
+        String MANIFEST = odir + "/META-INF/MANIFEST.MF";
+        String buildJar = "jar cvfmn " + jarPath.substring(0, jarPath.lastIndexOf(".") - 1) + "-new.jar " + MANIFEST + " -C " + odir + "/ .";
+        out.println(buildJar);
+        cmd(buildJar, (s) -> s.forEach(System.out::println));
     }
 }
